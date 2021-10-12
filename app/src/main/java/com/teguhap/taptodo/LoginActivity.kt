c634f8745adb879314b3af03d23fe82ef77b7788
sellysyifa <83294078+sellysyifa@users.noreply.github.com>
2021-10-13 03:38:47 +0700

    benerin background edit text, benerin posisi elemen LoginActivity.kt

--- app/src/main/java/com/teguhap/taptodo/LoginActivity.kt
+++ app/src/main/java/com/teguhap/taptodo/LoginActivity.kt
@@ -2,11 +2,14 @@ package com.teguhap.taptodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         window.statusBarColor = getColor(R.color.utama)
 
