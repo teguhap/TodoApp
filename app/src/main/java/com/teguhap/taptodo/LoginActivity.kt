2450004b03b4fce6608e1d209afa72d05e68022d
sellysyifa <83294078+sellysyifa@users.noreply.github.com>
2021-10-19 03:28:45 +0700

    Finished Register Authtentication & Login,made HomeActivity.kt

--- app/src/main/java/com/teguhap/taptodo/LoginActivity.kt
+++ app/src/main/java/com/teguhap/taptodo/LoginActivity.kt
@@ -1,19 +1,26 @@
 package com.teguhap.taptodo
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import com.teguhap.taptodo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
+    lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val loader = ProgressDialog(this)
+        mAuth = FirebaseAuth.getInstance()
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         window.statusBarColor = getColor(R.color.utama)
@@ -23,6 +30,37 @@ class LoginActivity : AppCompatActivity() {
                 Intent(this@LoginActivity,RegistrationActivity :: class.java).also {
                     startActivity(it)
                 }
+            }
+
+            btnLogin.setOnClickListener {
+                val email = etEmail.text.toString().trim()
+                val password = etPassword.text.toString().trim()
+
+                if(email.isEmpty()){
+                    etEmail.error = "Email is required"
+                }
+                if(password.isEmpty()){
+                    etPassword.error = "Password is required"
+                }else{
+                    loader.setMessage("Login in Progress")
+                    loader.setCanceledOnTouchOutside(false)
+                    loader.show()
+
+                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(
+                        OnCompleteListener {
+                            if(it.isSuccessful){
+                               val intent =  Intent(this@LoginActivity,HomeActivity :: class.java)
+                                startActivity(intent)
+                                finish()
+                                loader.dismiss()
+                            }else{
+                                val error = it.exception.toString()
+                                Toast.makeText(this@LoginActivity,"Login Failed $error",Toast.LENGTH_SHORT).show()
+                                loader.dismiss()
+                            }
+                        })
+                }
+
             }
 
 
