7df4ed0aef67bca90d0c95a852a40902797aa91c
sellysyifa <83294078+sellysyifa@users.noreply.github.com>
2021-10-25 01:06:29 +0700

    Finished DatePicker

--- app/src/main/java/com/teguhap/taptodo/HomeActivity.kt
+++ app/src/main/java/com/teguhap/taptodo/HomeActivity.kt
@@ -1,19 +1,23 @@
 package com.teguhap.taptodo
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.teguhap.taptodo.adapter.AdapterTodoListView
 import com.teguhap.taptodo.data.TodoList
 import com.teguhap.taptodo.databinding.ActivityHomeBinding
-import java.text.SimpleDateFormat
 import java.util.*
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityHomeBinding
     @SuppressLint("InflateParams", "SetTextI18n", "SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +25,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
         window.statusBarColor = getColor(R.color.start_utama_gradient)
 
-
+        //List To_do
         val todoListToday = mutableListOf<TodoList>(
             TodoList("Belanja Sayur Jam 10","22-10-2021",R.drawable.ic_belanja_green,false),
             TodoList("Kerjain tugas Riset Operasi","22-10-2021",R.drawable.ic_book_red,false),
@@ -36,16 +40,12 @@ class HomeActivity : AppCompatActivity() {
         )
 
         //Calendar
-        val month_date = SimpleDateFormat("MMM")
         val calendar = Calendar.getInstance(TimeZone.getDefault())
         val date = calendar.get(Calendar.DATE)
-        val day = calendar.getDisplayName(Calendar.DAY_OF_MONTH,Calendar.SHORT,Locale.getDefault())
         val month =  calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault())
         val year = calendar.get(Calendar.YEAR)
-
         val days = arrayOf("SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY")
-
-        val currentDay = days[calendar.get(Calendar.DAY_OF_WEEK)-1];
+        val currentDay = days[calendar.get(Calendar.DAY_OF_WEEK)-1]
 
         //Calender day,month,year
         binding.apply {
@@ -54,11 +54,17 @@ class HomeActivity : AppCompatActivity() {
             tvMonthYear.text = " $month $year"
         }
 
-
+        //Bottomshet Component
         val btmSheet = BottomSheetDialog(this)
         val viewBtmSheet = layoutInflater.inflate(R.layout.add_todo_sheet,null)
         val ivItemAddTodo :  ImageView = viewBtmSheet.findViewById(R.id.ivItemAddTodo)
+        val btnSubmitAddTodo = viewBtmSheet.findViewById<AppCompatButton>(R.id.btnAddTodo)
+        val etTitle =viewBtmSheet.findViewById<EditText>(R.id.etAddTitle)
+        val etDesc = viewBtmSheet.findViewById<EditText>(R.id.etAddDesc)
+        val etDate = viewBtmSheet.findViewById<EditText>(R.id.etAddDate)
+        val priority = viewBtmSheet.findViewById<RadioGroup>(R.id.rgPriority)
 
+        //Home Layout Component
         binding.apply {
             rvTodoToday.adapter = AdapterTodoListView(todoListToday)
             rvTodoToday.layoutManager = LinearLayoutManager(this@HomeActivity)
@@ -84,10 +90,64 @@ class HomeActivity : AppCompatActivity() {
                     setCanceledOnTouchOutside(true)
                     show()
                 }
-
             }
 
+            btnFood.setOnClickListener {
+                btmSheet.apply {
+                    setContentView(viewBtmSheet)
+                    ivItemAddTodo.setImageResource(R.drawable.item_food)
+                    setCanceledOnTouchOutside(true)
+                    show()
+                }
+            }
 
+            btnSport.setOnClickListener {
+                btmSheet.apply {
+                    setContentView(viewBtmSheet)
+                    ivItemAddTodo.setImageResource(R.drawable.item_sport)
+                    setCanceledOnTouchOutside(true)
+                    show()
+                }
+            }
+
+            btnDo.setOnClickListener {
+                btmSheet.apply {
+                    setContentView(viewBtmSheet)
+                    ivItemAddTodo.setImageResource(R.drawable.item_do)
+                    setCanceledOnTouchOutside(true)
+                    show()
+                }
+            }
+
+            btnOther.setOnClickListener {
+                btmSheet.apply {
+                    setContentView(viewBtmSheet)
+                    ivItemAddTodo.setImageResource(R.drawable.item_other)
+                    setCanceledOnTouchOutside(true)
+                    show()
+                }
+            }
+        }//Tutuo Binding Home Activity
+
+
+        etDate.setOnClickListener {
+            val tanggal = calendar.get(Calendar.DATE)
+            val bulan = calendar.get(Calendar.MONTH)
+            val tahun= calendar.get(Calendar.YEAR)
+            val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                etDate.setText("$dayOfMonth-$month-$year")
+            },tahun,bulan,tanggal)
+            datePicker.show()
+
+        }
+
+        btnSubmitAddTodo.setOnClickListener {
+            val title = etTitle.text.toString()
+            val desc = etDesc.text.toString()
+            val datePicked = etDate.text.toString()
+            Toast.makeText(this,"Title : $title,Desc : $desc",Toast.LENGTH_SHORT).show()
+            todoListToday.add(TodoList(title,datePicked,R.drawable.ic_book_red,false))
+            btmSheet.dismiss()
         }
     }
 }
